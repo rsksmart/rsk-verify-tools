41d8baa51daa53b53106b5ee3681b648d8ae01c8
emi <emiliorizzo@gmail.com>
2021-01-28 15:35:47 -0700

    Add cacheSolc

---
+++ src/cacheSolc.js
@@ -0,0 +1,12 @@
+const { cacheSolc } = require('@rsksmart/rsk-contract-verifier')
+const config = require('./lib/config')
+const log = require('./lib/log')
+const releasesOnly = !!process.argv[2]
+
+cacheSolc(config, { log, releasesOnly }).then(() => {
+  log.info('Done!')
+  process.exit(0)
+}).catch(err => {
+  log.error(err)
+  process.exit(9)
+})
